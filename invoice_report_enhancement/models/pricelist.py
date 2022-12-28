1a1f58bb494059e0890d85b350055d0fa10b7e45
Omnya Rashwan <o.reshwan@tatayab.com>
2022-12-28 17:56:06 +0200

    [MOD]

--- invoice_report_enhancement/models/pricelist.py
+++ invoice_report_enhancement/models/pricelist.py
@@ -7,10 +7,10 @@ class PriceList(models.Model):
 
     is_wholesale = fields.Boolean(string='not Wholesale')
 
-    @api.constrains('is_wholesale')
-    def check_is_wholesale(self):
-        for item in self:
-            price_lists = self.env['product.pricelist'].search([('is_wholesale', '=', True),
-                                                                ('id', '!=', item.id)])
-            if price_lists:
-                raise ValidationError(_('There is already wholesale Price List.'))
+    # @api.constrains('is_wholesale')
+    # def check_is_wholesale(self):
+    #     for item in self:
+    #         price_lists = self.env['product.pricelist'].search([('is_wholesale', '=', True),
+    #                                                             ('id', '!=', item.id)])
+    #         if price_lists:
+    #             raise ValidationError(_('There is already wholesale Price List.'))
