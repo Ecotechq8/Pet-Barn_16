8d63bc993c5cf412962ec06b8fba9de893476709
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 <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>
2023-01-03 17:42:09 +0300

    Add

--- partner_customization/__manifest__.py
+++ partner_customization/__manifest__.py
@@ -4,14 +4,22 @@
     "version": "15.0.1.0.0",
     "category": "contact",
     "author": "Elsayed Iraky",
-    "depends": ["base","stock"],
+    "depends": ["base","stock","point_of_sale"],
     "data": [
         "security/ir.model.access.csv",
         "views/res_partner_view.xml",
         "views/pet_product_type.xml",
-        
         "views/product_template.xml",
     ],
+
+    'assets': {
+        'point_of_sale.assets': [
+            'partner_customization/static/src/js/models.js',
+            'partner_customization/static/src/xml/bi_pos_stock.xml',
+            'partner_customization/static/src/css/choices.css',
+
+        ]
+    },
 
     "installable": True,
 }
